e5fc8ddbdde609850004a2119f2db54d4c7a58bd
Akash3444 <akashmoradiya3444@gmail.com>
2022-01-21 10:11:30 +0530

    add new styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,18 @@
 module.exports = {
   content: ["./*.{html,js}"],
-  darkMode: 'class',
+  darkMode: "class",
   theme: {
     extend: {
       fontFamily: {
-        inter: "'Inter', sans-serif"
+        inter: "'Inter', sans-serif",
       },
       boxShadow: {
-        around: '0 0 80px 0px theme(colors.gray.200)',
-        'around-dark': '0 0 80px 0px theme(colors.gray.700)',
-        'around-lg': '0 0 100px 0px theme(colors.gray.300)'
-      }
+        around: "0 0 80px 0px theme(colors.gray.200)",
+        "around-dark": "0 0 80px 0px theme(colors.gray.700)",
+        "around-lg": "0 0 100px 0px theme(colors.gray.300)",
+        "no-blur": "0 2px 0 1px theme(colors.gray.200)",
+      },
     },
   },
   plugins: [],
-}
+};
